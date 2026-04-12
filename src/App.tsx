e92768c0960d808f09f2be4b4ec7448a52df3248
HaniNghi <vmmn27@gmail.com>
2026-04-12 15:54:37 +0300

    add: add Routes for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css'
 import QuizDetailPage from './pages/QuizDetailPage'
+import Container from "@mui/material/Container";
+import { BrowserRouter, Routes, Route } from "react-router";
+import QuizList from './pages/QuizList'
+import CssBaseline from "@mui/material/CssBaseline";
+
 
 function App() {
   return (
-    <QuizDetailPage />
+<Container>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<QuizList/>}/>
+      <Route path="/quiz-details" element={<QuizDetailPage />}/>
+    </Routes>
+  </BrowserRouter>
+      <CssBaseline />
+</Container>
+
   )
 }
 
