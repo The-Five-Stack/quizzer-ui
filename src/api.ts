f809bdf5fe30a6fa142a82b79914764bdc77664e
HaniNghi <vmmn27@gmail.com>
2026-04-13 20:54:35 +0300

    feat(fetchQuizzesWithAuth): create new function to get all quizzes

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,47 @@
-export const fetchQuiz = () => {
-  //fetch call & response handling
-  return fetch(import.meta.env.VITE_API_BASE_URL + "/api/quizzes").then((response) => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const fetchQuizzWithAuth = async (options: RequestInit = {}) => {
+  const defaultHeaders = {
+    'Authorization': 'Basic dGVhY2hlcjp0ZWFjaGVyMTIz', // Tài khoản: teacher / Mật khẩu: teacher123
+    'Content-Type': 'application/json',
+  };
+
+  const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/quizzes`, {
+    ...options,
+    headers: { ...defaultHeaders, ...options.headers },
+  }).then((response) => {
     if (!response.ok) 
         throw new Error("Error when fetching quizzes.");
     return response.json();
   });
-};
 
+  return response;
+};
