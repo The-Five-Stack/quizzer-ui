d19f8e89ff36224c3361590035525e03f8d232b0
Oanh Pham <oanh.pham@myy.haaga-helia.fi>
2026-04-26 15:37:03 +0300

    feat(category): add category list page for student dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import AddQuestionForm from "./pages/AddQuestionForm"
 import AddAnswerForm from "./pages/AddAnswerForm";
 import CategoryList from "./pages/CategoryList";
+import CategoryListStudent from "./pages/CategoryListStudent";
 /**
  * Root App component that sets up the SPA routing structure.
  * Routes:
@@ -28,6 +29,10 @@ function App() {
           <Route path="/quizzes/:id/questions" element={<AddQuestionForm />} />
           <Route path="/questions/:questionId/answers" element={<AddAnswerForm />} />
           <Route path="/categories" element={<CategoryList />} />
+          
+          <Route path="/student/categories" element={<CategoryListStudent />} />
+          {/* Sadik, pls insert the correct element into the line below to naviagate Category list page student to Quizz by category page */}
+          {/* <Route path="/student/categories/:id/published-quizzes" element={<.... />} /> */}
         </Routes>
       </BrowserRouter>
       <CssBaseline />
