037195ee205b036e20fd0219ef5f4edd87b34ea1
HaniNghi <vmmn27@gmail.com>
2026-04-27 18:37:13 +0300

    update: give param value when navigate

--- src/pages/CategoryListStudent.tsx
+++ src/pages/CategoryListStudent.tsx
@@ -25,6 +25,7 @@ export default function CategoryListStudent() {
                         fontWeight: "500", 
                         cursor: "pointer"
                     }}
+                    state={{ categoryName: params.value }}
                 >
                     {params.value}
                 </Link>
