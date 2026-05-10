541049338e0c09ec8589a8aaae4768e47514dfc8
HaniNghi <vmmn27@gmail.com>
2026-05-10 10:40:30 +0300

    fix: put <p> into a div

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,7 +1,7 @@
 import "../index.css";
-import DeleteIcon from '@mui/icons-material/Delete';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import DeleteIcon from "@mui/icons-material/Delete";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 /**
  * Props for QuizCard component
  */
@@ -42,21 +42,24 @@ export default function QuizCard(props: QuizCardProps) {
         </div>
 
         {/* Created date display */}
-        <p>
-          <b>Created:</b> {new Date(props.createdAt).toLocaleDateString("en-GB")}
-        </p>
-
-        {/* Course code display */}
-        <p>
-          <b>Code:</b> {props.courseCode}
-        </p>
-
-        {/* Category display, categoryName should be conditionally rendered */}
-        {props.categoryName && (
+        <div className="qc-p">
           <p>
-            <b>Category:</b> {props.categoryName}
+            <b>Created:</b>{" "}
+            {new Date(props.createdAt).toLocaleDateString("en-GB")}
           </p>
-        )}
+
+          {/* Course code display */}
+          <p>
+            <b>Code:</b> {props.courseCode}
+          </p>
+
+          {/* Category display, categoryName should be conditionally rendered */}
+          {props.categoryName && (
+            <p>
+              <b>Category:</b> {props.categoryName}
+            </p>
+          )}
+        </div>
 
         {/* Action buttons */}
         <div className="qc-foot">
@@ -72,7 +75,11 @@ export default function QuizCard(props: QuizCardProps) {
           <div>
             {/* Toggle publish status button */}
             <button className="qc-icon" onClick={props.onToggleStatus}>
-              {props.published === true ? <VisibilityOffIcon fontSize="small" /> : <VisibilityIcon fontSize="small" />}
+              {props.published === true ? (
+                <VisibilityOffIcon fontSize="small" />
+              ) : (
+                <VisibilityIcon fontSize="small" />
+              )}
             </button>
             {/* Delete button */}
             <button className="qc-icon del" onClick={props.onDelete}>
